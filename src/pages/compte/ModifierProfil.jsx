2e6105664501c179465c1646f13f6fc6c0600602
OneKamer <contact@onekamer.co>
2025-11-16 19:35:19 +0100

    PROD: fix upload avatar (avatars folder)

--- src/pages/compte/ModifierProfil.jsx
+++ src/pages/compte/ModifierProfil.jsx
@@ -59,7 +59,7 @@ const ModifierProfil = () => {
         { type: file.type || "image/jpeg" }
       );
       formData.append("file", safeFile);
-      formData.append("type", "profiles");
+      formData.append("type", "avatars");
       formData.append("recordId", user.id);
 
       const res = await fetch("https://onekamer-server.onrender.com/api/upload-media", {
